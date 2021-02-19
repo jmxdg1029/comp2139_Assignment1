75dcb00d461ab340aaf463706154cd76bb69f358
Ruzzel Orejola <ruzzel.orejola@gmail.com>
2021-02-19 16:40:11 -0500

    Productmodel

---
+++ Assignment1/Assignment1/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment1.Models
+{
+    public class Product
+    {
+
+        [RegularExpression(@"\d"), Required(ErrorMessage = "Please enter a value for the product number")]
+        public int ProductNumber { get; set; }
+
+        [RegularExpression(@"\D"), Required(ErrorMessage = "Please enter the a value for the product name")]
+        public String ProductName { get; set; }
+
+        [RegularExpression(@"\d"), Required(ErrorMessage = "Please enter a value for the product number")]
+        public int ProductPrice { get; set; }
+    }
+}
