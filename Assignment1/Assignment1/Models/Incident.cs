c1be4e5cfd4a828707712f5018c47e8da1f359ac
Gabriel Silva <Gabriel.silva58@hotmail.com>
2021-02-20 21:54:41 -0500

    Added Incident

---
+++ Assignment1/Assignment1/Models/Incident.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Incident
+    {
+        public int IncidentId { get; set; }
+        
+        [Required (ErrorMessage = "Please enter the customers name.")]
+        public string CustomerName { get; set; }
+        
+        [Required(ErrorMessage = "Please enter the products name.")]
+        public string Product { get; set; }
+        
+        [Required(ErrorMessage ="Please enter in a date.")]
+        public DateTime DateOpened { get; set; }
+        
+
+    }
+}
---
+++ Assignment1/Assignment1/Models/IncidentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class IncidentContext : DbContext
+    {
+        public IncidentContext(DbContextOptions<IncidentContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Incident> Incidents { get; set; }
+    }
+}
