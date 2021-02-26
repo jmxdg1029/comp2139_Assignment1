aa10c23bec3fe7e99939c36a675f1363e6a5123c
John Michael De Guzman <jmxdg1029@gmail.com>
2021-02-25 21:40:56 -0500

    awda

--- Assignment1/Assignment1/Models/Technician.cs
+++ Assignment1/Assignment1/Models/Technician.cs
@@ -9,10 +9,13 @@ namespace Assignment1.Models
     public class Technician
     {
         public int TechnicianId { get; set; }
+
         [Required(ErrorMessage = "Please enter a valid Name")]
-        public string? TechnicianName { get; set; }
+        public string TechnicianName { get; set; }
+
         [Required(ErrorMessage = "Please enter a valid Email Address")]
         public string TechnicianEmail { get; set; }
+
         [Required(ErrorMessage = "Please enter a valid Phone Number")]
         public string TechnicianPhone { get; set; }
        
