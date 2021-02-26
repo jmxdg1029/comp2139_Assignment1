f2a0d55103df34b08e7faa777b9ec87aac292aca
John Michael De Guzman <jmxdg1029@gmail.com>
2021-02-26 18:02:49 -0500

    awdawd

--- Assignment1/Assignment1/Models/Incident.cs
+++ Assignment1/Assignment1/Models/Incident.cs
@@ -13,13 +13,13 @@ namespace Assignment1.Models
 
         [Range(1, 1000, ErrorMessage = "Please enter the customers name.")]
         public int CustomerId { get; set; }
-        public Customer Customer { get; set; }
+       
      
 
         [Range(1, 1000, ErrorMessage = "Enter a product name")]
 
         public int ProductId { get; set; }
-        public Product Product { get; set; }
+        
       
 
         [Required(ErrorMessage = "Please enter the title of the incident.")]
@@ -29,8 +29,7 @@ namespace Assignment1.Models
         [Range(1, 1000, ErrorMessage = "Please enter the technicians name")]
 
         public int TechnicianId { get; set; }
-        public Technician Technician { get; set; }
-    
+       
 
         public string Description { get; set; }
 
